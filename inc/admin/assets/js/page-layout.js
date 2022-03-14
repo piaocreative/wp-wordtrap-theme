55b00ef90bd6f16bb152736f80c858beba634549
sefgda8 <sefgda8@gmail.com>
2022-03-14 15:21:57 -0700

    add the page layout in admin

---
+++ inc/admin/assets/js/page-layout.js
@@ -0,0 +1,51 @@
+/**
+ * Wordtrap Page Layouts
+ * @since 1.0.0.
+ */
+( function( $ ) {
+  
+  // Open layout options
+  $( 'body' ).on( 'click', '.layout-box .block', function() {
+    var $this = $( this ),
+      $layout_box = $this.closest( '.layout-box' ),
+      block = $this.data( 'block' ),
+      $options = $( $( '#wordtrap-layout-' + block + '-options-html' ).html() );
+
+    $options.appendTo( $layout_box.find( '.block-options' ) );
+    $layout_box.find( '.block-options' ).data( 'block', block );
+    $layout_box.addClass( 'open-options' );
+
+    $options.find( '.builder-blocks' ).each( function() {
+      var $block = $( this );
+
+      if ( $block.data( 'value' ) ) {
+        $block.val( $block.data( 'value' ) );
+      }
+    } );
+  } );
+
+  // Close layout options
+  $( 'body' ).on( 'click', '.layout-header .back', function( e ) {
+    e.preventDefault();
+
+    var $this = $( this ),
+      $layout_box = $this.closest( '.layout-box' );
+
+    if ( ! $layout_box.hasClass( 'open-options' ) ) {
+      return;
+    }
+    
+    var $block_options = $layout_box.find('.block-options')
+      block = $block_options.data( 'block' );
+
+    $block_options.find( '.builder-blocks' ).each( function() {
+      var $block = $( this );
+      $block.data( 'value', $block.val() );
+    } );
+
+    $( '#wordtrap-layout-' + block + '-options-html' ).html( $block_options.html() );
+    $block_options.data( 'block', '' ).html( '' );
+    $layout_box.closest( '.layout-box ').removeClass( 'open-options' );
+  } );
+
+} )( jQuery );
--- inc/admin/templates/page-layout.php
+++ inc/admin/templates/page-layout.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Wordtrap admin page layout page
  *
@@ -8,121 +9,237 @@
 
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
-?>
 
+// Get system status
+
+global $wp_filesystem;
+// Initialize the WordPress filesystem, no more using file_put_contents function
+if ( empty( $wp_filesystem ) ) {
+  require_once ABSPATH . '/wp-admin/includes/file.php';
+  WP_Filesystem();
+}
+
+$data = array(
+  'wp_uploads'     => wp_get_upload_dir (),
+  'memory_limit'   => wp_convert_hr_to_bytes( @ini_get( 'memory_limit' ) ),
+  'time_limit'     => ini_get( 'max_execution_time' ),
+  'max_input_vars' => ini_get( 'max_input_vars' ),
+);
+
+$status = array(
+  'uploads'        => wp_is_writable( $data['wp_uploads']['basedir'] ),
+  'fs'             => ( $wp_filesystem || WP_Filesystem() ) ? true : false,
+  'zip'            => class_exists( 'ZipArchive' ),
+  'suhosin'        => extension_loaded( 'suhosin' ),
+  'memory_limit'   => $data['memory_limit'] >= 268435456,
+  'time_limit'     => ( ( $data['time_limit'] >= 600 ) || ( 0 == $data['time_limit'] ) ) ? true : false,
+  'max_input_vars' => $data['max_input_vars'] >= 2000,
+);
+
+?>
 <div class="wrap wordtrap-wrap">
-  <h2 class="screen-reader-text"><?php _e('Dashboard', 'wordtrap'); ?></h2>
+  <h2 class="screen-reader-text"><?php _e( 'Page Layout', 'wordtrap' ); ?></h2>
   <?php
   wordtrap_get_template_part(
-    'inc/admin/pages/header',
+    'inc/admin/templates/header',
     null,
     array(
-      'active_item' => 'wordtrap-page-layout',
-      'title'       => __('Page Layout', 'wordtrap'),
-      'subtitle'    => __('Create page layout and assign them to different pages with display condition.', 'wordtrap'),
+      'active_item' => 'wordtrap',
+      'title'       => __( 'Page Layout', 'wordtrap' ),
+      'subtitle'    => __( 'Create page layout and assign them to different pages with display condition.', 'wordtrap' ),
     )
   );
   ?>
-  <main>
-    <div class="page-layout">
-      <div class="layout-box">
-        <h3 class="layout-header">
-          <a href="#" class="back"><i class="fas fa-arrow-left"></i></a>
-          Page Layout for Template Bulider
-        </h3>
-        <div class="layout wordtrap-layout">
-          <div class="popup layout-part" data-part="popup">
-            <p><?php _e('Popup', 'wordtrap') ?></p>
-          </div>
-          <div class="layout-part" data-part="top-block">
-            <p><?php _e('Top', 'wordtrap') ?></p>
-          </div>
-          <div class="layout-part" data-part="header">
-            <p><?php _e('Header', 'wordtrap') ?></p>
-          </div>
-          <div class="layout-part" data-part="banner-block">
-            <p><?php _e('Banner', 'wordtrap') ?></p>
-          </div>
-          <div class="layout-part" data-part="main-top-block">
-            <p><?php _e('Main Top', 'wordtrap') ?></p>
-          </div>
-          <div class="content-wrapper">
-            <div class="left-sidebar layout-part" data-part="left-sidebar">
-              <p><?php _e('Left Sidebar', 'wordtrap') ?></p>
+  <main class="row">
+    <div class="col-left">
+      <div class="page-layout">
+        <div class="layout-box">
+          <h3 class="layout-header">
+            <a href="#" class="back"><i class="dashicons dashicons-arrow-left-alt"></i></a>
+            <?php _e( 'Page Layout for Template Bulider', 'wordtrap' ) ?>
+          </h3>
+          <div class="layout wordtrap-layout">
+            <div class="block" data-block="header">
+              <p><?php _e( 'Header', 'wordtrap' ) ?></p>
             </div>
-            <div class="content">
-              <div class="block layout-part" data-part="main-inner-top-block">
-                <p><?php _e('Main Inner Top', 'wordtrap') ?></p>
+            <div class="main-wrap">
+              <div class="block sidebar" data-block="left-sidebar">
+                <p><?php _e( 'Left Sidebar', 'wordtrap' ) ?></p>
               </div>
-              <div class="block layout-part">
-                <p><?php _e('Main', 'wordtrap') ?></p>
+              <div class="content-wrap">
+                <div class="block" data-block="content">
+                  <p><?php _e( 'Content', 'wordtrap' ) ?></p>
+                </div>
               </div>
-              <div class="block layout-part" data-part="main-inner-bottom-block">
-                <p><?php _e('Main Inner Bottom', 'wordtrap') ?></p>
+              <div class="block sidebar" data-block="right-sidebar">
+                <p><?php _e( 'Right Sidebar', 'wordtrap' ) ?></p>
               </div>
             </div>
-            <div class="right-sidebar layout-part" data-part="right-sidebar">
-              <p><?php _e('Right Sidebar', 'wordtrap') ?></p>
+            <div class="block" data-block="footer">
+              <p><?php _e( 'Footer', 'wordtrap' ) ?></p>
             </div>
           </div>
-          <div class="layout-part" data-part="main-bottom-block">
-            <p><?php _e('Main Bottom', 'wordtrap') ?></p>
+          <div class="block-options">
           </div>
-          <div class="layout-part" data-part="footer">
-            <p><?php _e('Footer', 'wordtrap') ?></p>
-          </div>
-          <div class="layout-part" data-part="bottom-block">
-            <p><?php _e('Bottom', 'wordtrap') ?></p>
-          </div>
-        </div>
-        <div class="part-options">
         </div>
       </div>
-    </div>
 
-    <?php
-    $parts = array( 
-      'popup', 
-      'top-block', 
-      'header', 
-      'banner-block', 
-      'main-top-block', 
-      'left-sidebar',
-      'main-inner-top-block', 
-      'main-inner-bottom-block', 
-      'right-sidebar', 
-      'main-bottom-block', 
-      'footer',
-      'bottom-block'
-    );
-    foreach ( $parts as &$part ) :
-      ob_start();
-      $backup_part = $part;
-      if ( in_array( $part, array( 'top-block', 'banner-block', 'main-top-block', 'main-inner-top-block', 'main-inner-bottom-block', 'main-bottom-block', 'bottom-block' ) ) ) {
-        $part = 'block';
-      }
-      $this->add_control( 'note', $this->options[$part]['note'] );
-      if ( ! in_array( $part, array( 'left-sidebar', 'right-sidebar') ) ) :
-        foreach ( $this->template_list[$part] as $page_id => $page_title ) {
-          $conditions = get_post_meta( $page_id, '_porto_builder_conditions', true );
-          $block_pos = get_post_meta( $page_id, '_porto_block_pos', true );
-          if ( !empty( $conditions ) 
-            && ( ( $part == 'block' && !empty( $block_pos ) && 'block_' . $backup_part == $block_pos ) || ( 'block' != $part ) ) ) {
-            $this->add_control( 'builder-blocks', $this->options[$part]['builder-blocks'], $page_id );
+      <?php
+      // Load templates
+      $blocks = array ( 
+        'header', 
+        'left-sidebar',
+        'content',
+        'right-sidebar', 
+        'footer',
+      );
+      foreach ( $blocks as $key => $block ) :
+        ob_start ();
+        
+        $this->add_control( 'note', $this->options[$block]['note'] );
+        
+        if ( isset( $this->options[$block]['builder-blocks'] ) ) :
+          foreach ( $this->template_list[$block] as $page_id => $page_title ) {
+            $conditions = get_post_meta( $page_id, $this->meta_conditions, true );
+            
+            if ( ! empty( $conditions ) ) {
+              $this->add_control( 'builder-blocks', $this->options[$block]['builder-blocks'], $page_id );
+            }
           }
-        }
-        $this->add_control( 'builder-blocks', $this->options[$part]['builder-blocks'], 'preset' );
+          $this->add_control( 'builder-blocks', $this->options[$block]['builder-blocks'], 'preset' );
+          ?>
+          <div class="add-new-layout">
+            <a href="#"><?php _e( 'Add New Layout Condition', 'wordtrap' ) ?></a>
+          </div>
+          <?php
+        endif;
+        $output = ob_get_clean();
         ?>
-        <div class="add-new-layout">
-          <a href="#">Add New Layout Condition</a>
-        </div>
-        <?php
-      endif;
-      $output = ob_get_clean();
-      ?>
-      <script type="text/template" id="porto-layout-<?php echo esc_attr( $backup_part ); ?>-options-html">
-        <?php echo porto_filter_output( $output ); ?>
-      </script>
-    <?php endforeach; ?>
+        <script type="text/template" id="wordtrap-layout-<?php echo esc_attr( $block ); ?>-options-html">
+          <?php echo $output; ?>
+        </script>
+      <?php endforeach; ?>
+    </div>
+    <div class="col-right">
+      <h3><?php _e( 'System Status', 'wordtrap' ); ?></h3>
+      <div class="wordtrap-system-status">
+        <ul class="system-status">
+          <li>
+            <?php if ( $status['uploads'] ) : ?>
+              <i class="status-yes dashicons dashicons-yes"></i>
+            <?php else : ?>
+              <i class="status-no dashicons dashicons-no"></i>
+            <?php endif; ?>
+            <span class="label"><?php _e( 'Uploads folder writable', 'wordtrap' ); ?></span>
+            <?php if ( ! $status['uploads'] ) : ?>
+              <p class="status-error">
+                <?php _e( 'Uploads folder must be writable. Please set write permission to your wp-content/uploads folder.', 'wordtrap' ) ?>
+              </p>
+            <?php endif; ?>
+          </li>
+          <li>
+            <?php if ( $status['fs'] ) : ?>
+              <i class="status-yes dashicons dashicons-yes"></i>
+            <?php else : ?>
+              <i class="status-no dashicons dashicons-no"></i>
+            <?php endif; ?>
+            <span class="label"><?php _e( 'WP File System', 'wordtrap' ); ?></span>
+            <?php if ( ! $status['fs'] ) : ?>
+              <p class="status-error">
+                <?php _e( 'File System access is required for pre-built websites and plugins installation. Please contact your hosting provider.', 'wordtrap' ) ?>
+              </p>
+            <?php endif; ?>
+          </li>
+
+          <li>
+            <?php if ( $status['zip'] ) : ?>
+              <i class="status-yes dashicons dashicons-yes"></i>
+            <?php else : ?>
+              <i class="status-no dashicons dashicons-no"></i>
+            <?php endif; ?>
+            <span class="label"><?php _e( 'ZipArchive', 'wordtrap' ); ?></span>
+            <?php if ( ! $status['zip'] ) : ?>
+              <p class="status-error">
+                <?php _e( 'ZipArchive is required for pre-built websites and plugins installation. Please contact your hosting provider.', 'wordtrap' ) ?>
+              </p>
+            <?php endif; ?>
+          </li>
+
+          <?php if ( $status['suhosin'] ) : ?>
+            <li>
+              <i class="status-info dashicons dashicons-info"></i>
+              <span class="label"><?php _e( 'SUHOSIN Installed', 'wordtrap' ); ?></span>
+              <p class="status-notice">
+                <?php _e( 'Suhosin may need to be configured to increase its data submission limits.', 'wordtrap' ) ?>
+              </p>
+            </li>
+          <?php else : ?>
+            <li>
+              <?php if ( $status['memory_limit'] ) : ?>
+                <i class="status-yes dashicons dashicons-yes"></i>
+              <?php else : ?>
+                <?php if ( $data['memory_limit'] < 134217728 ) : ?>
+                  <i class="status-no dashicons dashicons-bell"></i>
+                <?php else : ?>
+                  <i class="status-info dashicons dashicons-info"></i>
+                <?php endif; ?>
+              <?php endif; ?>
+              <span class="label"><?php _e( 'PHP memory_limit', 'wordtrap' ); ?> <em> (<?php echo size_format( $data['memory_limit'] ); ?>)</em></span>
+              <?php if ( ! $status['memory_limit'] ) : ?>
+                <?php if ( $data['memory_limit'] < 134217728 ) : ?>
+                  <p class="status-error">
+                    <?php _e( 'Minimum <strong>128 MB</strong> is required, <strong>256 MB</strong> is recommended.', 'wordtrap' ) ?>
+                  </p>
+                <?php else : ?>
+                  <p class="status-error">
+                    <?php _e( 'Current memory limit is OK, however <strong>256 MB</strong> is recommended.', 'wordtrap' ) ?>
+                  </p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </li>
+            <li>
+              <?php if ( $status['time_limit'] ) : ?>
+                <i class="status-yes dashicons dashicons-yes"></i>
+              <?php else : ?>
+                <?php if ( $data['time_limit'] < 300 ) : ?>
+                  <i class="status-no dashicons dashicons-bell"></i>
+                <?php else : ?>
+                  <i class="status-info dashicons dashicons-info"></i>
+                <?php endif; ?>
+              <?php endif; ?>
+              <span class="label"><?php _e( 'PHP max_execution_time', 'wordtrap' ); ?> <em>(<?php echo esc_html( $data['time_limit'] ); ?>)</em></span>
+              <?php if ( ! $status['time_limit'] ) : ?>
+                <?php if ( $data['time_limit'] < 300 ) : ?>
+                  <p class="status-error">
+                    <?php _e( 'Minimum <strong>300</strong> is required, <strong>600</strong> is recommended.', 'wordtrap' ) ?>
+                  </p>
+                <?php else : ?>
+                  <p class="status-error">
+                    <?php _e( 'Current time limit is OK, however <strong>600</strong> is recommended.', 'wordtrap' ) ?>
+                  </p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </li>
+            <li>
+              <?php if ( $status['max_input_vars'] ) : ?>
+                <i class="status-yes dashicons dashicons-yes"></i>
+              <?php else : ?>
+                <i class="status-no dashicons dashicons-bell"></i>
+              <?php endif; ?>
+              <span class="label"><?php _e( 'PHP max_input_vars', 'wordtrap' ); ?> <em>(<?php echo esc_html( $data['max_input_vars'] ); ?>)</em></span>
+              <?php if ( ! $status['max_input_vars'] ) : ?>
+                <p class="status-error">
+                  <?php _e( 'Minimum 2000 is required.', 'wordtrap' ) ?>
+                </p>
+              <?php endif; ?>
+            </li>
+            <li class="info">
+              <i class="status-yes dashicons dashicons-editor-help"></i>
+              <?php _e( 'php.ini values are shown above. Real values may vary, please check your limits using <a target="_blank" href="http://php.net/manual/en/function.phpinfo.php" rel="noopener noreferrer">php_info()</a>', 'wordtrap' ) ?>
+            </li>
+          <?php endif; ?>
+        </ul>
+      </div>
+    </div>
   </main>
 </div>
